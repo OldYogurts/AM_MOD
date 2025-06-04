7f1516affd5ff995dc33099d703eb80e220941bd
OldYogurts <kasis.antonis@gmail.com>
2025-06-04 13:16:20 +0300

    test 4

--- TEST_GIT.py
+++ TEST_GIT.py
@@ -3,13 +3,6 @@
 # THIS IS THE MASTER 
 	
 def myfunc() :
-	print("i am the master + hotfix");
+	print("i am the master");
 
-	empty line is_not empty
-
-
-def my_mom_is_fat():
-	print("FAT FAT");
-
-def FAT_AGAIN():
-	print("another dev stuff");
+	empty line
